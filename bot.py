ae2ecf543df8181840174dff4b3a8930b0fd3fa2
Egor <lotus9200@gmail.com>
2023-06-02 22:44:08 +0600

    Добавил возможность запуска бота в режими лоу поллинг

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import database
 
 from data.config import Telegram, Webhooks
@@ -12,6 +14,8 @@ from handlers.routers import user_router, admin_router
 
 from middlewares.base_middleware import MyMiddleware
 
+
+# Webhook method
 async def on_startup(dispatcher: Dispatcher, bot: Bot):
     await database.load_database()
     print('database loaded')
@@ -41,4 +45,32 @@ if __name__ == '__main__':
     setup_application(application, dispatcher, bot=bot)
     run_app(application, host=Webhooks.listen_address, port=Webhooks.listen_port)
 
-
+# Long polling method
+# async def on_startup(dispatcher: Dispatcher, bot: Bot):
+#     await database.load_database()
+#     print('database loaded')
+#     dispatcher.message.outer_middleware(MyMiddleware())
+#     dispatcher.callback_query.outer_middleware(MyMiddleware())
+#     print('middleware loaded')
+#     dispatcher.include_router(user_router)
+#     dispatcher.include_router(admin_router)
+#     print('routers included')
+#     print('[!] Bot started!')
+#
+#     await start_bot(dispatcher, bot)
+#
+# async def start_bot(dispatcher: Dispatcher, bot: Bot):
+#     await delete_webhook(bot)
+#     await dispatcher.start_polling(bot)
+#
+# async def delete_webhook(bot: Bot):
+#     await bot.delete_webhook()
+#
+# if __name__ == '__main__':
+#     bot = Bot(token=Telegram.token, parse_mode='HTML')
+#     storage = MemoryStorage()
+#     dispatcher = Dispatcher(bot=bot, storage=storage)
+#
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     loop.run_until_complete(on_startup(dispatcher, bot))
